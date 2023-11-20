081fb0779439126e13616a842cf74d2290c7afda
ghs <3171392318@qq.com>
2023-11-21 06:53:32 +0800

    完善初级聊天室

--- WebSocket-chatroom/app.js
+++ WebSocket-chatroom/app.js
@@ -1,25 +1,44 @@
 const  ws = require('nodejs-websocket');
 const PORT = 3000
+const  TYPE_ENTER = 0
+const TYPE_LEAVE = 1
+const TYPE_MSG = 2
 
-//记录当前连接上来的用户总数
 let count = 0;
 
-//conn 每个连接到服务器的用户都会有一个conn对象
+/**
+ * 分析：
+ *     消息不应该是一个简单的字符串
+ *      这个消息应该是一个对象
+ *      type：消息的类型，0:表示进入聊天室的消息，1：用户离开聊天室的消息，2：正常的聊天消息
+ *      msg：消息的内容
+ *      time:聊天的具体时间。
+ */
+
 const server  = ws.createServer(conn=>{
     count ++;
     conn.userName = `用户${count}`;
     console.log(`${conn.userName}加入了聊天室`)
-    //告诉所有用户，有人加入了聊天室
-    broadcast(`${conn.userName}进入了聊天室`);
+    broadcast({
+        type:TYPE_ENTER,
+        msg: `${conn.userName}进入了聊天室`,
+        time:new Date().toLocaleTimeString()
+    });
 
     conn.on('text',data=>{
-        //当我们接收到某个用户的消息的时候，告诉所有用户，发送的消息内容是什么？
-        broadcast(`${conn.userName}:${data}`);
+        broadcast({
+            type:TYPE_MSG,
+            msg: `${conn.userName}: ${data}`,
+            time:new Date().toLocaleTimeString()
+        });
     })
     conn.on('close',data=>{
         count--;
-        //告诉所有的用户，有人离开了聊天室
-        broadcast(`${conn.userName}离开了聊天室`)
+        broadcast({
+            type:TYPE_LEAVE,
+            msg: `${conn.userName}离开了聊天室`,
+            time:new Date().toLocaleTimeString()
+        });
     })
 
     conn.on('error',data=>{
@@ -29,9 +48,8 @@ const server  = ws.createServer(conn=>{
 })
 
 function broadcast(msg){
-    //server.connections:表示所有的用户
     server.connections.forEach(item=>{
-        item.send(msg)
+        item.send(JSON.stringify(msg))
     })
 }
 
